4d83ea62be4ce750003a7feec55432356e33c936
Kayvan <kvakili813@gmail.com>
2018-10-04 15:31:54 -0400

    adds columns to films table

--- db/migrate/20181004191330_create_films.rb
+++ db/migrate/20181004191330_create_films.rb
@@ -1,4 +1,10 @@
 class CreateFilms < ActiveRecord::Migration[5.2]
   def change
+    create_table :films do |t|
+      t.string :title
+      t.integer :year
+      t.integer :user_id
+      t.integer :genre_id
+    end
   end
 end
