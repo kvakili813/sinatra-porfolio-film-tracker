fe84a7c0d1ec39ce93eef8413e6b7171251f0ad7
Kayvan <kvakili813@gmail.com>
2018-10-04 17:19:21 -0400

    adds post /signup method to userscontroller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,7 +8,20 @@ class UsersController < ApplicationController
     end
   end
 
-  
+  post '/signup' do
+    @new_user = User.new(username: params[:username].downcase, password: params[:password])
+
+    if params[:username].match(/^[a-zA-Z0-9_-]*$/) == nil
+      flash[:message] = 'Only letters, numbers, underscore(_) and hyphen(-) are allowed for Username!'
+      redirect '/signup'
+    elsif @new_user.save
+     session[:user_id] = @new_user.id
+     redirect '/'
+   else
+     flash[:message] = 'Fill all the fields'
+     redirect '/signup'
+   end
+ end
 
 
 
